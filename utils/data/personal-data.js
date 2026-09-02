174828dceea2abb4fc02e267677c7f0aae829495
patricioimbrogno <patricioimbrogno@gmail.com>
2026-09-02 12:52:39 -0300

    fix: change image

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Patricio Imbrogno",
-  profile: '/profilePatricioImbrogno.png',
+  profile: '/portfolioImbrogno.png',
   designation: "Full Stack Developer",
   description: "My name is Patricio Imbrogno, and I’m a Full Stack Developer with 3+ years of experience building web applications with React, TypeScript, JavaScript, Node.js, and AWS. I have experience working in agile environments, developing scalable solutions, integrating APIs and cloud services, and working with microservices architectures. With a background in communications and journalism, I bring strong problem-solving, analytical, and communication skills to software development. I’m passionate about technology, continuous learning, and building meaningful digital products.",
   email: 'patricioimbrogno@gmail.com',
